c2d4500076999cd6a62294a86128501040a484cf
Ting <tihuang@microsoft.com>
2017-03-20 18:22:41 -0400

    docker

---
+++ src/Agent.Worker/Docker/DockerInfo.cs
@@ -0,0 +1,8 @@
+namespace Microsoft.VisualStudio.Services.Agent.Worker.Docker
+{
+    public class DockerInfo
+    {
+        public string ContainerId { get; set; }
+        public string ContainerName { get; set; }
+    }
+}
